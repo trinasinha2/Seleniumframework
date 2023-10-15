c7a98083a9f6ee97be2da95f5c2a62a06b9af866
Trina <145247818+trinasinha2@users.noreply.github.com>
2023-10-15 23:54:28 +0530

    Update BrowserFactory.java
    update

--- src/main/java/com/learnautomation/factory/BrowserFactory.java
+++ src/main/java/com/learnautomation/factory/BrowserFactory.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
