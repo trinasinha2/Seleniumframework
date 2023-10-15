3b94931b02190d1adc04ba939252fc13dd877ec9
Trina <145247818+trinasinha2@users.noreply.github.com>
2023-10-16 00:10:35 +0530

    Update BrowserFactory.java

--- src/main/java/com/learnautomation/factory/BrowserFactory.java
+++ src/main/java/com/learnautomation/factory/BrowserFactory.java
@@ -1,14 +1,14 @@
 package com.learnautomation.factory;
 
 import java.time.Duration;
-
+import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.remote.RemoteWebDriver;
 import com.learnautomation.dataProvider.ConfigReader;
 
 public class BrowserFactory 
